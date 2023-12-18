f57dc81949765d920f7449d3a2250bc920ac22d2
krzychu124 <krzychu124@gmail.com>
2023-12-18 21:33:12 +0100

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using Game;
+using Game.Modding;
+using JetBrains.Annotations;
+
+namespace Traffic
+{
+    [UsedImplicitly]
+    public class Mod : IMod
+    {
+
+        public void OnCreateWorld(UpdateSystem updateSystem) {
+            Logger.Info(nameof(OnCreateWorld));
+        }
+
+        public void OnDispose() {
+            Logger.Info(nameof(OnDispose));
+        }
+
+        public void OnLoad() {
+            Logger.Info(nameof(OnLoad));
+        }
+    }
+}

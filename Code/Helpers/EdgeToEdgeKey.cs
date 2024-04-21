c67f341bffe65a85975b35d60a9403ee4e4bf0c3
krzychu124 <krzychu124@gmail.com>
2024-04-22 01:51:53 +0200

    EdgeToEdgeKey helper struct

---
+++ Code/Helpers/EdgeToEdgeKey.cs
@@ -0,0 +1,36 @@
+using System;
+using Unity.Entities;
+
+namespace Traffic.Helpers
+{
+    internal struct EdgeToEdgeKey : IEquatable<EdgeToEdgeKey>
+    {
+        public Entity edge1;
+        public Entity edge2;
+
+        public EdgeToEdgeKey(Entity edge1, Entity edge2)
+        {
+            this.edge1 = edge1;
+            this.edge2 = edge2;
+        }
+
+
+        public bool Equals(EdgeToEdgeKey other)
+        {
+            return edge1.Equals(other.edge1) && edge2.Equals(other.edge2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EdgeToEdgeKey other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (edge1.GetHashCode() * 397) ^ edge2.GetHashCode();
+            }
+        }
+    }
+}
